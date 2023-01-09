064e7c2f3e266f52d138038038ee060d3736e118
winstxnhdw <winstxnhdw@gmail.com>
2023-01-10 02:13:12 +0800

    feat: add waitress for production builds

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
+from typing import Literal
+
 from flask import Flask
 from flask_cors import CORS
+from waitress import serve
 
 from app.config import Config
 
 
 class App:
 
+    HTTPMethods = Literal['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
     flask = Flask(__name__)
     CORS(flask)
 
@@ -13,9 +17,20 @@ class App:
     config.from_object(Config)
     config['CORS_HEADERS'] = 'Content-Type'
 
+    get = flask.get
+    post = flask.post
+    put = flask.put
+    delete = flask.delete
+    patch = flask.patch
+
 
     def run(debug: bool | None, load_dotenv: bool=True, **options):
 
+        if not debug:
+            print(f'Waitress serving on http://{App.config["HOST"]}:{App.config["PORT"]}')
+            serve(App.flask, host=App.config['HOST'], port=App.config['PORT'])
+            return
+        
         App.flask.run(
             host=App.config['HOST'],
             port=App.config['PORT'],
@@ -25,6 +40,6 @@ class App:
         )
 
 
-    def route(rule: str, **options):
+    def route(rule: str, methods: list[HTTPMethods]=['GET'], **options):
 
-        return App.flask.route(rule, **options)
+        return App.flask.route(rule, methods=methods, **options)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,6 +3,8 @@ from app.routes.index import index
 
 def initialise_routes():
 
-    [print(f'Initialising {route.__name__} route') for route in (
+    print('\nROUTES')
+    [print(f' * {route.__name__} route found') for route in [
         index
-    )]
+    ]]
+    print('')
