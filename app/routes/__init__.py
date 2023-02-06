f0e198e4dc20972f0d8134e094d949d16052cbc6
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2023-02-06 08:21:30 +0800

    perf: map is slower than a for-loop

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,20 +2,6 @@ from importlib import import_module
 from os import listdir
 
 
-def import_route(file_name: str):
-    """
-    Summary
-    -------
-    import a route module
-
-    Parameters
-    ----------
-    file_name (str) : name of the route module to import
-    """
-    module_name = import_module(f"app.routes.{file_name[:-3]}").__name__
-    print(f' * {module_name} route found')
-
-
 def initialise_routes():
     """
     Summary
@@ -24,11 +10,13 @@ def initialise_routes():
     """
     print('\nROUTES')
 
-    module_names = (
+    module_file_names = [
         file_name for file_name in listdir('app/routes')
         if not file_name.startswith('__init__') and file_name.endswith('.py')
-    )
+    ]
 
-    list(map(import_route, module_names))
+    for file_name in module_file_names:
+        module_name = import_module(f"app.routes.{file_name[:-3]}").__name__
+        print(f' * {module_name} route found')
 
     print('')
